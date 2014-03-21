70bebb560db576f701f9f14b6e78ef9f09218408
Micki Chow <Micki@Mickis-MacBook-Pro.local>
2014-03-21 12:00:21 +0100

    Initial Commit

---
+++ Git Test/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Git Test
+//
+//  Created by Micki Chow on 21/03/14.
+//  Copyright (c) 2014 Corredor ApS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
